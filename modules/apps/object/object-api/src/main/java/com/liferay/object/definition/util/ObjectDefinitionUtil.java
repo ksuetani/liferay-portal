0c9f04e384ad5aaa1148583e1238cb79b5dd6b87
pei-jung <pei-jung.lan@liferay.com>
2026-05-18 21:34:14 -0700

    LPD-88843 Add SEOStudioGSCCredentialEntry object definition

--- modules/apps/object/object-api/src/main/java/com/liferay/object/definition/util/ObjectDefinitionUtil.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/definition/util/ObjectDefinitionUtil.java
@@ -285,6 +285,8 @@ public class ObjectDefinitionUtil {
 		).put(
 			"SEOStudioDomain", "/seo-studio/domains"
 		).put(
+			"SEOStudioGSCCredentialEntry", "/seo-studio/gsc-credential-entries"
+		).put(
 			"SEOStudioInsightType", "/seo-studio/insight-types"
 		).put(
 			"SEOStudioInstance", "/seo-studio/instances"
