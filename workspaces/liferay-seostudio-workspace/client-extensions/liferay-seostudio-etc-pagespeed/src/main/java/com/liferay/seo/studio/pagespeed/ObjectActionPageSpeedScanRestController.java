cfd57ea8784d3e271e674ab6e84e46abc1d7645e
Kiana Suetani <kiana.suetani@liferay.com>
2026-06-11 17:16:57 -0700

    LPD-88837 Add object action REST controller to dispatch PageSpeed scans for both DESKTOP and MOBILE strategies

---
+++ workspaces/liferay-seostudio-workspace/client-extensions/liferay-seostudio-etc-pagespeed/src/main/java/com/liferay/seo/studio/pagespeed/ObjectActionPageSpeedScanRestController.java
@@ -0,0 +1,117 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.seo.studio.pagespeed;
+
+import com.liferay.client.extension.util.spring.boot3.BaseRestController;
+import com.liferay.client.extension.util.spring.boot3.client.LiferayOAuth2AccessTokenManager;
+import com.liferay.seo.studio.pagespeed.scanner.LiferayHeadlessClient;
+import com.liferay.seo.studio.pagespeed.scanner.PageSpeedScanner;
+
+import java.net.http.HttpClient;
+
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.json.JSONObject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Kiana Suetani
+ */
+@RequestMapping("/object/action/pagespeed")
+@RestController
+public class ObjectActionPageSpeedScanRestController
+	extends BaseRestController {
+
+	@PostMapping
+	public ResponseEntity<String> post(@RequestBody String json) {
+		if (_log.isDebugEnabled()) {
+			_log.debug(json);
+		}
+
+		JSONObject jsonObject = new JSONObject(json);
+
+		JSONObject objectEntryJSONObject = jsonObject.getJSONObject(
+			"objectEntry");
+
+		JSONObject valuesJSONObject = objectEntryJSONObject.getJSONObject(
+			"values");
+
+		long domainId = valuesJSONObject.optLong(
+			"r_seoStudioDomainToSEOStudioScans_seoStudioDomainId", 0);
+
+		if (domainId <= 0) {
+			return new ResponseEntity<>(
+				"Scan is missing a domain", HttpStatus.BAD_REQUEST);
+		}
+
+		try {
+			String portalBaseURL =
+				lxcDXPServerProtocol + "://" + lxcDXPMainDomain;
+
+			LiferayHeadlessClient liferayHeadlessClient =
+				new LiferayHeadlessClient(
+					_liferayOAuth2AccessTokenManager.getTokenValue(
+						PageSpeedConstants.OAHS_EXTERNAL_REFERENCE_CODE),
+					_httpClient, portalBaseURL);
+
+			String[] domainInfo = liferayHeadlessClient.getDomainInfo(domainId);
+
+			_pageSpeedScanner.scanAsync(
+				domainInfo[0], _httpClient, _liferayOAuth2AccessTokenManager,
+				pageSpeedScanResults -> {
+					if (_log.isInfoEnabled()) {
+						_log.info(
+							"PageSpeed scan completed for " +
+								pageSpeedScanResults.size() + " strategies");
+					}
+				},
+				errorMessage -> _log.error(
+					"PageSpeed scan failed: " + errorMessage),
+				domainInfo[1], portalBaseURL, () -> null,
+				List.of(
+					PageSpeedConstants.STRATEGY_DESKTOP,
+					PageSpeedConstants.STRATEGY_MOBILE));
+
+			return ResponseEntity.ok(
+			).build();
+		}
+		catch (Exception exception) {
+			if (exception instanceof InterruptedException) {
+				Thread thread = Thread.currentThread();
+
+				thread.interrupt();
+			}
+
+			_log.error("Unable to start PageSpeed scan", exception);
+
+			return new ResponseEntity<>(
+				"Unable to start PageSpeed scan",
+				HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	private static final Log _log = LogFactory.getLog(
+		ObjectActionPageSpeedScanRestController.class);
+
+	private static final HttpClient _httpClient = HttpClient.newHttpClient();
+
+	@Autowired
+	private LiferayOAuth2AccessTokenManager _liferayOAuth2AccessTokenManager;
+
+	@Autowired
+	private PageSpeedScanner _pageSpeedScanner;
+
+}
