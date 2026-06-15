1234ce1145796dcbb6c6d5eb00ef319feb1a80fd
Kiana Suetani <kiana.suetani@liferay.com>
2026-06-15 08:32:25 -0700

    LPD-94550 Add admin configurations page with integrations card

---
+++ modules/dxp/apps/seo-studio/seo-studio-web/src/main/resources/META-INF/resources/js/integrations/cell_renderers/IntegrationStatusCellRenderer.tsx
@@ -0,0 +1,35 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import ClayLabel from '@clayui/label';
+import {Provider} from '@clayui/provider';
+import React from 'react';
+
+interface IProps {
+	itemData?: {
+		state?: {
+			key?: string;
+			name?: string;
+		};
+	};
+}
+
+export default function IntegrationStatusCellRenderer({itemData}: IProps) {
+	const state = itemData?.state;
+
+	if (!state) {
+		return null;
+	}
+
+	const displayType = state.key === 'active' ? 'success' : 'danger';
+
+	return (
+		<Provider spritemap={Liferay.Icons.spritemap}>
+			<ClayLabel displayType={displayType}>
+				{state.name?.toUpperCase()}
+			</ClayLabel>
+		</Provider>
+	);
+}
